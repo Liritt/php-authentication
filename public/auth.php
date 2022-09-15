79033aac7d394ba274f5f3df009e6e4fab1dfc0b
Lemont Gaétan <gaetan.lemont@etudiant.univ-reims.fr>
2022-09-15 11:55:52 +0200

    Formulaire + authentification réussies

---
+++ public/auth.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Authentication\UserAuthentication;
+use Authentication\Exception\AuthenticationException;
+use Html\Helper\Dumper;
+use Html\WebPage;
+
+$authentication = new UserAuthentication();
+
+$p = new WebPage('Authentification');
+
+try {
+    // Tentative de connexion
+    $user = $authentication->getUserFromAuth();
+    $p->appendContent(
+        <<<HTML
+<div>Bonjour {$user->getFirstName()}</div>
+HTML
+    );
+} catch (AuthenticationException $e) {
+    // Récupération de l'exception si connexion échouée
+    $p->appendContent("Échec d'authentification&nbsp;: {$e->getMessage()}");
+} catch (Exception $e) {
+    $p->appendContent("Un problème est survenu&nbsp;: {$e->getMessage()}");
+}
+
+// Envoi du code HTML au navigateur du client
+echo $p->toHTML();
--- src/Authentication/UserAuthentication.php
+++ src/Authentication/UserAuthentication.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Authentication;
 
+use Authentication\Exception\AuthenticationException;
+use Entity\Exception\EntityNotFoundException;
+use Entity\User;
+
 class UserAuthentication
 {
     private const LOGIN_INPUT_NAME = 'login';
@@ -14,11 +18,23 @@ class UserAuthentication
         $const1 = self::LOGIN_INPUT_NAME;
         $const2 = self::PASSWORD_INPUT_NAME;
         return <<<HTML
-                <form action="$action">
-                    <input type="text" placeholder="{$const1}">
-                    <input type="text" placeholder="{$const2}">
+                <form action="$action" method="post">
+                    <input type="text" name="{$const1}" placeholder="{$const1}">
+                    <input type="text" name="{$const2}" placeholder="{$const2}">
                     <input type="submit" value="{$submitText}">
                 </form>
 HTML;
+    }
+
+    /**
+     * @throws AuthenticationException
+     * @throws EntityNotFoundException
+     */
+    public function getUserFromAuth(): User
+    {
+        if (!User::findByCredentials($_POST[self::LOGIN_INPUT_NAME], $_POST[self::PASSWORD_INPUT_NAME])) {
+            throw new AuthenticationException("Cet utilisateur n'existe pas");
+        }
+        return User::findByCredentials($_POST[self::LOGIN_INPUT_NAME], $_POST[self::PASSWORD_INPUT_NAME]);
     }
 }
