2975f814e41b2ac301a119b7adfd3282fa523c4f
Lemont Gaétan <gaetan.lemont@etudiant.univ-reims.fr>
2022-09-26 15:23:52 +0200

    avatar.php fonctionnel

--- public/avatar.php
+++ public/avatar.php
@@ -1,11 +1,21 @@
 <?php
+
 declare(strict_types=1);
 
 use Entity\Exception\EntityNotFoundException;
 use Entity\UserAvatar;
 
+$avatar = null;
+
 try {
-    $userId = $_GET['id'];
-    $avatar = UserAvatar::findById($userId);
+    if (isset($_GET['userId'])) {
+        $userId = $_GET['userId'];
+        $avatar = UserAvatar::findById((int) $userId)->getAvatar();
+    }
 } catch (EntityNotFoundException $e) {
 }
+
+if ($avatar === null) {
+    $avatar = file_get_contents('default_avatar.png');
+}
+
