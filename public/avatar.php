11cf41a4d75daf08e30b871e1e2fdce3f2c7201d
Gaétan Lemont <gaetan.lemont@etudiant.univ-reims.fr>
2022-09-26 21:32:19 +0200

    Simplification ET correction de avatar.php

--- public/avatar.php
+++ public/avatar.php
@@ -5,17 +5,11 @@ declare(strict_types=1);
 use Entity\Exception\EntityNotFoundException;
 use Entity\UserAvatar;
 
-$avatar = null;
-
 try {
-    if (isset($_GET['userId'])) {
+    if (isset($_GET['userId']) && ctype_digit($_GET['userId'])) {
         $userId = $_GET['userId'];
-        $avatar = UserAvatar::findById((int) $userId)->getAvatar();
+        echo UserAvatar::findById((int) $userId)->getAvatar();
     }
 } catch (EntityNotFoundException $e) {
+    echo file_get_contents('default_avatar.png');
 }
-
-if ($avatar === null) {
-    $avatar = file_get_contents('default_avatar.png');
-}
-
